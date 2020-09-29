41c9c49d83ac74239411298b7b9ca348dc95ff8d
Kellen Barber <kellenbarber@gmail.com>
2020-09-28 22:02:25 -0700

    webpack and babel set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,38 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
 	entry: {
-		server: './server/main.js',
-		client: './client/index.js'
+		server: "./server/main.js",
+		client: "./client/index.js"
 	},
 	output: {
-		filename: '[name].js',
-		path: __dirname + '/dist'
+		filename: "[name].js",
+		path: __dirname + "/dist"
 	},
 	module: {
 		rules: [
 			{
-				test: /\.(js|jsx)$/,
+				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {
 					loader: "babel-loader"
 				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: "html-loader"
+					}
+				]
 			}
 		]
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./client/index.html",
+			filename: "./index.html"
+		})
+	]
 }; 
