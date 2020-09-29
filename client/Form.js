cf6c3d5a3a19c88be0dca541b5ee87ab744ce60e
Kellen Barber <kellenbarber@gmail.com>
2020-09-29 13:28:26 -0700

    add runtime plugin for babel so that I can use async/await

---
+++ client/Form.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Form = props => {
+  const [value, setValue] = useState("");
+
+  const getData = async () => {
+    const url = "http://localhost:5000/test";
+    const response = await fetch(url);
+    const data = await response.text();
+    return data;
+  }
+
+  const handleClick = async () => {
+    const data = await getData();
+    console.log("data: ", data);
+    setValue(data);
+  }
+
+  return (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={handleClick}>CLICK THE BUTTON</button>
+    </div>
+  );
+}
+
+export default Form;
