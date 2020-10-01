940291a4edd88aff7c1278ed3dd81dde7415b60e
Kellen Barber <kellenbarber@gmail.com>
2020-10-01 14:03:30 -0700

    another Form test; update classname in Form header

--- client/Form.js
+++ client/Form.js
@@ -19,7 +19,7 @@ const Form = props => {
 
   return country ? (
     <div>
-        <h1 className={"test"}>{`You are in ${country}`}</h1>
+        <h1 className={"countryHeader"}>{`You are in ${country}`}</h1>
         <label for="money">Please enter an amount of money in your local currency:</label>
         <input type="text" name="money" onChange={handleChange}></input>
         <button onClick={handleClick}>Get BigMac Data</button>
--- client/Form.test.js
+++ client/Form.test.js
@@ -11,3 +11,9 @@ it("should return null without props", () => {
     const form = shallow(<Form />);
     expect(form.type()).toEqual(null);
 });
+
+it("should return render header correctly with 'United States' as country prop", () => {
+    const form = shallow(<Form country={"United States"}/>);
+    expect(form.type()).not.toEqual(null);
+    expect(form.find('.countryHeader').text()).toEqual("You are in United States");
+});
