c9ee83a77b5fe573920d9957037fe0bf4dc9df9e
Kellen Barber <kellenbarber@gmail.com>
2020-10-01 21:10:43 -0700

    remove unnecessary check

--- client/RandomCountryResults.js
+++ client/RandomCountryResults.js
@@ -15,13 +15,13 @@ const RandomCountryResults = props => {
     // (Calculation is [INPUT] * (local dollar price / RANDCOUNTRY dollar price)) 
     const currencyValue = amount * (localDollarPrice / bigMacData.DollarPrice);
 
-    return props.bigMacData ? (
+    return (
         <div className={"randomCountryResults"}>
             <h2>Random Country: </h2>
             <p>You could buy {Math.floor(numberOfBigMacs)} of Big Macs in {bigMacData.Country} with {amount}!</p>
             <p>Your {amount} is worth about {currencyValue.toFixed(2)} in {bigMacData.Country}.</p>
         </div>
-    ) : null;
+    );
 };
 
 export default RandomCountryResults;
